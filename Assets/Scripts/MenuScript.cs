bc5851f85790108804b2602386d31e55cf8ac027
YoichiMasa <laurence.jamil@gmail.com>
2015-02-26 14:13:34 -0500

    Setup MenuScript

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public GameObject startButton;
+	public GameObject optionsButton;
+	public GameObject exitButton;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
