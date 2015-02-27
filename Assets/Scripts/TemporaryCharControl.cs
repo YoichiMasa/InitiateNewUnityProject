09cf22b9ee72f847466da62a7f0417f50460d9ba
YoichiMasa <laurence.jamail@gmail.com>
2015-02-27 13:12:23 -0500

    Combined Projects
    Added Dan's Level to Build

---
+++ Assets/Scripts/TemporaryCharControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TemporaryCharControl : MonoBehaviour {
+
+	public float moveSpeed = 15f;
+
+	private Vector3 input;
+	public Rigidbody movable;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		movable.AddForce(input*moveSpeed);
+	}
+}
