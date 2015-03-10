0ff84e876c2949bcc5d23549646ec0c1e904fafe
YoichiMasa <laurence.jamail@gmail.com>
2015-03-10 10:21:06 -0400

    Played with Zenject
    Decided not to use it, still need to implement a global game controller

---
+++ Assets/Scripts/InventoryScripts/IInventory.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IInventory
+{
+
+	void addItem(Item item);
+
+	void removeItem(Item item);
+
+	void useItem(Item item);
+
+	void checkWeight(Item item);
+
+	
+}
