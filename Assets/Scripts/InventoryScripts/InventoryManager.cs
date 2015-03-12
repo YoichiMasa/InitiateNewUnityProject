72d4a75e569b0ef0bec4296cb5c57ab26f33063b
YoichiMasa <laurence.jamail@gmail.com>
2015-03-12 01:02:28 -0400

    Implemented Inventory/Testing Save/Load

--- Assets/Scripts/InventoryScripts/InventoryManager.cs
+++ Assets/Scripts/InventoryScripts/InventoryManager.cs
@@ -3,73 +3,71 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class InventoryManager : MonoBehaviour {
-	private IList<Item> inventory;
-	public int invWeight = 0;
-	public int MAX_WEIGHT = 25;
+	public static IList<Item> inventory;
+	public static InventoryManager invent;
+	public static Inventory spawn;
 
-	protected InventoryManager()
-	{
-		inventory = new List<Item>();
-	}
-	
-	private static InventoryManager instance = null;
-	
-	public static InventoryManager getInstance
-	{
-		get
-		{
-			if (InventoryManager.instance == null)
-			{
-				instance = GameObject.FindObjectOfType<InventoryManager>();
-				//InventoryManager.instance = new InventoryManager();
-			}
-			return instance;
-		}
-	}
+	public static int invWeight = 0;
+	public static int MAX_WEIGHT = 25;
 
 	void Awake()
 	{
-		if(instance == null)
+		spawn = GameObject.FindGameObjectWithTag ("Inventory").GetComponent<Inventory> ();
+		if (invent == null) 
 		{
-			instance = this;
-			DontDestroyOnLoad(this);
+			DontDestroyOnLoad(gameObject);
+			inventory = new List<Item>();	
+			invent = this;
 		}
 		else
 		{
-			if(this != instance)
+			if( invent != this)
 			{
-				Destroy(this.gameObject);
+				Destroy(gameObject);
 			}
 		}
 	}
 
-	public void addItem(Item newItem)
-	{
 
-		Item temp;
-		if(!checkWeight(newItem))
-		if (newItem.itemType == Item.ItemType.Weapon) 
+	public static void addItem(Item item)
+	{
+		Item temp = (Item)ScriptableObject.CreateInstance<Item> ();
+		temp = item;
+		Debug.Log ("Name: " + temp.itemName);
+		if(!checkWeight(temp))
 		{
-			temp = (Weapon) newItem;
-			inventory.Add(temp);
-			invWeight = invWeight+temp.itemWeight;
+			if (temp.itemType == Item.ItemType.Weapon) 
+			{
+				temp = temp as Weapon;
+				inventory.Add(item);
+				spawn.addPrefab(item);
+				//invWeight = invWeight+temp.itemWeight;
+			}
+			else if (temp.itemType == Item.ItemType.Food) 
+			{
+				temp = temp as Food;
+				inventory.Add(temp);
+				invWeight = invWeight+temp.itemWeight;
+			}
+			else if (temp.itemType == Item.ItemType.KeyItem) 
+			{
+				temp = temp as KeyItem;
+				inventory.Add(temp);
+				invWeight = invWeight+temp.itemWeight;
+			}
 		}
-		else if (newItem.itemType == Item.ItemType.Food) 
+		else
 		{
-			temp = (Food) newItem;
-			inventory.Add(temp);
-			invWeight = invWeight+temp.itemWeight;
-		}
-		else if (newItem.itemType == Item.ItemType.KeyItem) 
-		{
-			temp = (KeyItem) newItem;
-			inventory.Add(temp);
-			invWeight = invWeight+temp.itemWeight;
+			Debug.Log ("Bag too Heavy");
 		}
 
 	}
 
-	public bool checkWeight(Item newItem)
+	public static void removeItem(Item currItem)
+	{
+	}
+
+	public static bool checkWeight(Item newItem)
 	{
 		Item temp = newItem;
 		bool full = false;
--- Assets/Scripts/InventoryScripts/Item.cs
+++ Assets/Scripts/InventoryScripts/Item.cs
@@ -15,18 +15,19 @@ public class Item : ScriptableObject {
 	public string itemDesc = "";
 	public int itemWeight = 0;
 	public ItemType itemType;
+	public Object itemSprite;
 
-	public Item()
-	{}
-
-	public void ConfigureItem(string name, int ID, string description, int baseWeight, int height, int width, ItemType type)
+	public virtual void ConfigureItem(string name, int ID, string description, int baseWeight, ItemType type, Object sprite)
 	{
 		itemName = name;
 		itemId = ID;
 		itemDesc = description;
 		itemWeight = baseWeight;
 		itemType = type;
+		itemSprite = sprite;
 	}
+
+	public virtual void useItem(){}
 }
 
 
