93ccc2381e4f27ef8a15f1d089ef5b6f20d0e04e
YoichiMasa <laurence.jamail@gmail.com>
2015-03-12 12:40:58 -0400

    Broke it

---
+++ Assets/Scripts/CameraSwitchTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitchTrigger : MonoBehaviour {
+
+	public Camera newCamera;
+	public Camera previousCamera;
+	
+	public CameraController switcher;
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		newCamera.enabled = true;
+		previousCamera.enabled = false;
+		switcher.currentCamera = newCamera;
+	}
+}
